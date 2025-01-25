a10677fe4f5102ea0f32407871fa1d868e0222a3
McCoy <emmanuelokpere911@gmail.com>
2025-01-25 11:05:36 +0100

    updated

--- task_app/urls.py
+++ task_app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path('update/<int:task_id>/', views.update_task, name='update_task'),
     path('confirm-delete/<int:task_id>/', views.confirm_delete_task, name='confirm_delete_task'),
-    path('views/', views.views_by_task, name='views_by_task'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('add/', views.add_task, name='add_task'),
     path('task_detail/<int:task_id>/', views.task_detail, name='task_detail'),
